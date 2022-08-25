40c3987d8e9a106372818fb9b91bb304aed35986
kvbwork <kvbdev@yandex.ru>
2022-08-25 16:55:58 +0500

    Plain HTTP requests support, added Authorization

--- src/main/java/com/cyber/HttpProxyServer.java
+++ src/main/java/com/cyber/HttpProxyServer.java
@@ -6,20 +6,22 @@ import com.cyber.http.HttpRequestParser;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.locks.LockSupport;
 
 public class HttpProxyServer {
-    final static int BACKLOG_LENGTH = 50;
-    final static int SO_TIMEOUT = 60;
+    protected static final int BACKLOG_LENGTH = 50;
+    protected static final int SO_TIMEOUT = 60;
 
-    final InetAddress addr;
-    final int port;
-    final ServerSocket socket;
-    final AtomicBoolean doWork;
+    protected final InetAddress addr;
+    protected final int port;
+    protected final ServerSocket socket;
+    protected final AtomicBoolean doWork;
 
-    final HttpRequestParser httpRequestParser;
+    protected final HttpRequestParser httpRequestParser;
 
     public HttpProxyServer(InetAddress addr, int port) throws IOException {
         this.addr = addr;
@@ -50,34 +52,36 @@ public class HttpProxyServer {
             }
         }
     }
-
+    
     protected void onClientConnection(Socket socket) {
         try (InputStream in = socket.getInputStream();
              OutputStream out = socket.getOutputStream()
         ) {
             HttpRequest httpRequest = readHttpRequest(in);
-            System.out.println("NEW REQUEST:");
-            System.out.println(httpRequest.toFullRequest());
+            System.out.println("NEW REQUEST from " + socket);
 
+            if (httpRequest.isEmpty()) {
+                System.out.println("EMPTY request");
+                return;
+            }
+
+            System.out.println(httpRequest.toFullRequest());
 
             if (HttpRequest.METHOD_CONNECT.equals(httpRequest.getMethod())) {
                 handleConnectMethod(httpRequest, in, out);
             } else {
-                //handlePlainMethod(httpRequest, in, out);
-                String response = "HTTP/1.1 404 Not Found\r\n\r\n";
-                out.write(response.getBytes());
-                in.skip(in.available());
+                handlePlainMethod(httpRequest, in, out);
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         } finally {
+            System.out.println("close client socket: " + socket);
             try {
                 socket.close();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
-        System.out.println("close client socket: " + socket);
     }
 
     protected HttpRequest readHttpRequest(InputStream in) throws IOException {
@@ -89,62 +93,47 @@ public class HttpProxyServer {
     }
 
     protected void handleConnectMethod(HttpRequest httpRequest, InputStream in, OutputStream out) throws IOException {
-        String[] hostParts = httpRequest.getHost().split(":");
-        String host = hostParts[0];
-        int port = Integer.valueOf(hostParts[1]);
-        System.out.println("CONNECT to " + host + ", port: " + port);
+        if (!httpBasicAuth(httpRequest, in, out)) {
+            System.out.println("UNAUTHORIZED!");
+            return;
+        }
 
         try (
-                Socket remoteSocket = new Socket(host, port);
+                Socket remoteSocket = new Socket(httpRequest.getHost(), httpRequest.getPort());
                 InputStream remoteInput = remoteSocket.getInputStream();
                 OutputStream remoteOut = remoteSocket.getOutputStream()
         ) {
             System.out.println("connected: " + remoteSocket);
 
-            if (!httpBasicAuth(httpRequest, in, out)) {
-                System.out.println("UNAUTHORIZED!");
-                return;
-            }
-
-            new Thread(new StreamTransfer(() -> remoteSocket.isConnected(), remoteInput, out)).start();
+            new Thread(new StreamTransfer(() -> !remoteSocket.isClosed(), remoteInput, out)).start();
 
             String okClientResponse = "HTTP/1.1 200 OK\r\n\r\n";
             out.write(okClientResponse.getBytes());
-            System.out.println("RESPONSE:");
+            System.out.println("FOUND RESPONSE:");
             System.out.println(okClientResponse);
 
-            new StreamTransfer(() -> remoteSocket.isConnected(), in, remoteOut).run();
+            new StreamTransfer(() -> !remoteSocket.isClosed(), in, remoteOut).run();
 
             System.out.println("disconnect remote host: " + httpRequest.getHost() + " (" + remoteSocket + ")");
         }
-
     }
 
     protected void handlePlainMethod(HttpRequest httpRequest, InputStream in, OutputStream out) throws IOException {
-        URL url = new URL(httpRequest.getPath());
-        httpRequest.setPath(url.getPath());
+        if (!httpBasicAuth(httpRequest, in, out)) {
+            System.out.println("UNAUTHORIZED!");
+            return;
+        }
 
         try (
-                Socket remoteSocket = new Socket(httpRequest.getHost(), 80);
+                Socket remoteSocket = new Socket(httpRequest.getHost(), httpRequest.getPort());
                 InputStream remoteInput = remoteSocket.getInputStream();
                 OutputStream remoteOut = remoteSocket.getOutputStream()
         ) {
             System.out.println("connected to " + remoteSocket);
 
+            new Thread(new StreamTransfer(() -> !remoteSocket.isClosed(), remoteInput, out)).start();
             remoteOut.write(httpRequest.toFullRequest().getBytes());
-
-            while (remoteSocket.isConnected()) {
-                int inputSize = remoteInput.available();
-
-                if (inputSize == 0) {
-                    LockSupport.parkNanos(1000);
-                    continue;
-                }
-
-                byte[] buf = new byte[inputSize];
-                remoteInput.read(buf);
-                out.write(buf);
-            }
+            new StreamTransfer(() -> !remoteSocket.isClosed(), in, remoteOut).run();
 
             System.out.println("disconnect remote host: " + httpRequest.getHost() + " (" + remoteSocket + ")");
         }
@@ -155,13 +144,12 @@ public class HttpProxyServer {
         String authString = httpRequest.getHeaders().getOrDefault("Proxy-Authorization", "");
         String envAuthString = System.getenv("USER_AUTH");
 
-        System.out.println("start httpBasicAuth() with " + authString + " vs " + envAuthString);
         System.out.flush();
 
         if (authString.isEmpty()) {
             String unauthorizedResponse = "HTTP/1.1 407 Proxy Authentication Required\r\nProxy-Authenticate: Basic realm=\"CyberNet\"\r\n\r\n";
             out.write(unauthorizedResponse.getBytes());
-            System.out.println("RESPONSE:");
+            System.out.println("AUTH RESPONSE:");
             System.out.println(unauthorizedResponse);
 
             in.skip(in.available());
@@ -169,9 +157,10 @@ public class HttpProxyServer {
         }
 
         if (!envAuthString.equals(authString)) {
+            System.out.println("auth failed with " + authString);
             String unauthorizedResponse = "HTTP/1.1 403 Forbidden\r\n\r\n";
             out.write(unauthorizedResponse.getBytes());
-            System.out.println("RESPONSE:");
+            System.out.println("AUTH RESPONSE:");
             System.out.println(unauthorizedResponse);
 
             in.skip(in.available());
